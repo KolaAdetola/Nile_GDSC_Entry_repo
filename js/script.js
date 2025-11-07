92b8e474941def5c0a489c79af139910ea7cff56
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2025-11-07 20:17:57 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const light_mode = document.querySelector('.light_mode');
-const body = document.querySelector('.body');
-
-light_mode.onclick = () => {
-    
-    body.classList.toggle('dark_mode')
-}
